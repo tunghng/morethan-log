a5eafd58c08bc03b6dbd28aee9b0427db05e6ead
htungg <lehoangtung081003@gmail.com>
2023-11-25 15:50:26 +0700

    Chat bot commit

--- src/layouts/RootLayout/Scripts.tsx
+++ src/layouts/RootLayout/Scripts.tsx
@@ -17,7 +17,22 @@ const Scripts: React.FC = () => (
             });`}
         </Script>
       </>
+      
     )}
+    
+    <Script strategy="beforeInteractive" id="voiceflow">
+      {`(function(d, t) {
+          var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+          v.onload = function() {
+            window.voiceflow.chat.load({
+              verify: { projectID: '65619f79fc460970ea4951e0' },
+              url: 'https://general-runtime.voiceflow.com',
+              versionID: 'production'
+            });
+          }
+          v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+      })(document, 'script');`}
+    </Script>
   </>
 )
 
